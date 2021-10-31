3ff5c4e835ee7797acb797ba5ab051db03c03418
Roman <greenfoxer@gmail.com>
2021-10-31 12:20:40 +0300

    homework 9

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 import { createElement, getRandom } from "./utils.js";
 import { ATTACK, HIT } from "./constants.js";
 import { generateLog } from "./logging.js";
+import request from "./request.js";
 import Player from "./player.js";
 
 class Game{
@@ -97,43 +98,45 @@ class Game{
         return $player;
     }
 
-    start = () => {
+    start = async () => {
+
+        const allCharacters = await request.getAll();
+        const player = allCharacters[getRandom(allCharacters.length - 1)];
+        const enemy = await request.getRandomEnemy();    
+
         const p1 = new Player ({
             player : 1,
-            name : 'Kitana',
-            hp : 100,
-            img : 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+            ...player,
         });
         
         const p2 = new Player({
             player : 2,
-            name : 'Subzero',
-            hp : 100,
-            img : 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+            ...enemy,
         });
 
         //оказывается в эвент можно прокинуть так контекст
         //const instance = this;
         
-        this.$formFight.addEventListener('submit', function (event) {
+        this.$formFight.addEventListener('submit', async function (event) {
             event.preventDefault();
 
             //И получить его потом тут.
             //console.log('instance', instance);
 
-            const enemy = GameInstance.enemyAttack();
-            const player = {};
+            let playerChoise = {};
         
             for(let item of GameInstance.$formFight){
                 if ( item.checked && item.name === 'hit'){
-                    player.value = getRandom(HIT[item.value]);
-                    player.hit = item.value;
+                    //playerChoise.value = getRandom(HIT[item.value]);
+                    playerChoise.hit = item.value;
                 }
                 if ( item.checked && item.name === 'defence'){
-                    player.defense = item.value;
+                    playerChoise.defence = item.value;
                 }
                 item.checked = false;
             }
+
+            const {player1:player,player2:enemy} = await request.letFight(playerChoise);
         
             GameInstance.playerTurn(p1, p2.name, player, enemy)
             GameInstance.playerTurn(p2, p1.name, enemy, player);
